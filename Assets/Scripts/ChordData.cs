b6e89d043337f5457d807b37ddbf71c0cda15242
braydensoneill <braydensoneill@gmail.com>
2024-12-17 19:57:03 +0000

    New Chord Buttons: F, D, E, Em
    - Added new chord buttons with functionality
    - Chord buttons added: F, D, E, Em
    - KNOWN BUG: Only a small area in some of the button is able to be clicked on some chord buttons

--- Assets/Scripts/ChordData.cs
+++ Assets/Scripts/ChordData.cs
@@ -79,6 +79,7 @@ namespace guitar
         {
             // Order -> Alphabetical
 
+            // TODO: needs audio X4 audio files
             AddChord("B", new List<Note> {
                 new Note { noteX = -1, noteY = 1 },
                 new Note { noteX = 2, noteY = 2 },
@@ -105,7 +106,6 @@ namespace guitar
                 new Note { noteX = 3, noteY = 5 },
                 new Note { noteX = 2, noteY = 6 }
             });
-
             
             AddChord("E", new List<Note> {
                 new Note { noteX = 0, noteY = 1 },
@@ -140,7 +140,7 @@ namespace guitar
         private void AddMinorChords()
         {
             // Order -> Alphabetical
-            
+
             AddChord("Em", new List<Note> {
                 new Note { noteX = 0, noteY = 1 },
                 new Note { noteX = 2, noteY = 2 },
