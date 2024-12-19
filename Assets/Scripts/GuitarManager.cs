435f6b768e1d629fb21b3a77f392eeb55fbd1a20
braydensoneill <braydensoneill@gmail.com>
2024-12-19 06:47:53 +0000

    Code Cleanup: Code Manager
    - Strings text fields are now handled with an array
    - Setting currentChordText using a ternary operator

--- Assets/Scripts/GuitarManager.cs
+++ Assets/Scripts/GuitarManager.cs
@@ -14,15 +14,12 @@ namespace guitar
 
         // Current Guitar Info text fields
         public TextMeshProUGUI currentChordText;
-        public TextMeshProUGUI currentString1Text;
-        public TextMeshProUGUI currentString2Text;
-        public TextMeshProUGUI currentString3Text;
-        public TextMeshProUGUI currentString4Text;
-        public TextMeshProUGUI currentString5Text;
-        public TextMeshProUGUI currentString6Text;
 
         // Strings
         public GameObject[] strings;
+
+        // Current Guitar Info text fields
+        public TextMeshProUGUI[] currentStringTexts;
 
         // Current notes
         [SerializeField] private Tuple<int, int>[] currentNotes;
@@ -39,12 +36,7 @@ namespace guitar
             // Set default chord initially
             currentChord = "default";
             currentChordText.text = "/";
-            currentString1Text.text = 0.ToString();
-            currentString2Text.text = 0.ToString();
-            currentString3Text.text = 0.ToString();
-            currentString4Text.text = 0.ToString();
-            currentString5Text.text = 0.ToString();
-            currentString6Text.text = 0.ToString();
+            ResetCurrentStringText();
             UpdateNotes();
         }
 
@@ -54,11 +46,8 @@ namespace guitar
             // Update the current chords state
             currentChord = newChord;
             
-            // Update the text that displays the current chord - " " if default chord
-            if(currentChord != "default")
-                currentChordText.text = currentChord;
-            else 
-                currentChordText.text = "/";
+            // Update the text that displays the current chord
+            currentChordText.text = (currentChord != "default") ? currentChord : "/";
 
             // Update notes to adhere to the new current Chord
             UpdateNotes();
@@ -75,13 +64,21 @@ namespace guitar
                 currentNotes[i] = strings[i].GetComponent<StringManager>().GetCurrentNote(currentChord);
             }
 
-            // TBD (cleanup) - Display the x value for each string using text
-            currentString1Text.text = currentNotes[0].Item1.ToString();
-            currentString2Text.text = currentNotes[1].Item1.ToString();
-            currentString3Text.text = currentNotes[2].Item1.ToString();
-            currentString4Text.text = currentNotes[3].Item1.ToString();
-            currentString5Text.text = currentNotes[4].Item1.ToString();
-            currentString6Text.text = currentNotes[5].Item1.ToString();
+            // Update the text for each string
+            for (int i = 0; i < currentStringTexts.Length; i++)
+            {
+                // Update string text with the current note's x value
+                currentStringTexts[i].text = currentNotes[i].Item1.ToString();
+            }
+        }
+
+        private void ResetCurrentStringText()
+        {
+            // Reset all string text fields to "0" initially
+            foreach (var textField in currentStringTexts)
+            {
+                textField.text = "0";
+            }
         }
     }
 }
