0bc96a83a14122cfac8040ed472d8a2198d701b6
braydensoneill <braydensoneill@gmail.com>
2024-12-21 21:56:28 +0000

    New Asset Implementation | Hierarchy restructure

--- Assets/Scripts/GuitarManager.cs
+++ Assets/Scripts/GuitarManager.cs
@@ -18,8 +18,8 @@ namespace guitar
         // Strings
         public GameObject[] strings;
 
-        // Current Guitar Info text fields
-        public TextMeshProUGUI[] currentStringTexts;
+        // // Current Guitar Info text fields
+        // public TextMeshProUGUI[] currentStringTexts;
 
         // Current notes
         [SerializeField] private Tuple<int, int>[] currentNotes;
@@ -36,7 +36,7 @@ namespace guitar
             // Set default chord initially
             currentChord = "default";
             currentChordText.text = "/";
-            ResetCurrentStringText();
+            //ResetCurrentStringText();
             UpdateNotes();
         }
 
@@ -64,21 +64,21 @@ namespace guitar
                 currentNotes[i] = strings[i].GetComponent<StringManager>().GetCurrentNote(currentChord);
             }
 
-            // Update the text for each string
-            for (int i = 0; i < currentStringTexts.Length; i++)
-            {
-                // Update string text with the current note's x value
-                currentStringTexts[i].text = currentNotes[i].Item1.ToString();
-            }
+            // // Update the text for each string
+            // for (int i = 0; i < currentStringTexts.Length; i++)
+            // {
+            //     // Update string text with the current note's x value
+            //     currentStringTexts[i].text = currentNotes[i].Item1.ToString();
+            // }
         }
 
-        private void ResetCurrentStringText()
-        {
-            // Reset all string text fields to "0" initially
-            foreach (var textField in currentStringTexts)
-            {
-                textField.text = "0";
-            }
-        }
+        // private void ResetCurrentStringText()
+        // {
+        //     // Reset all string text fields to "0" initially
+        //     foreach (var textField in currentStringTexts)
+        //     {
+        //         textField.text = "0";
+        //     }
+        // }
     }
 }
