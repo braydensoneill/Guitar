c539ed13f6b21408a8a4eb19dca496bd1a390016
braydensoneill <braydensoneill@gmail.com>
2025-01-25 15:05:52 +0000

    ChordButton Hold-down functionality
    - Chord buttons used to change the current chord now behave like string buttons
    - Button now activates when user holds down and moves within the bounds of the chord button
    - No longer HAVE to click it

--- Assets/Scripts/ChordButton.cs
+++ Assets/Scripts/ChordButton.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace guitar
 {
-    public class ChordButton : MonoBehaviour
+    public class ChordButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
     {
-        [Tooltip("Name of the chord this button sets.")]
+        private bool canActivate = true;
+
         public string chordName; // The chord to set when this button is clicked.
 
         private GuitarManager guitarManager;
 
-        void Start()
+        // This method is called when the GameObject is enabled
+        void OnEnable()
         {
             // Find the first instance of GuitarManager in the scene
             guitarManager = Object.FindFirstObjectByType<GuitarManager>();
@@ -20,8 +23,30 @@ namespace guitar
             }
         }
 
-        // This method is called when the button is clicked
-        public void OnButtonClick()
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (Input.GetMouseButton(0) && canActivate)
+            {
+                ActivateButton();
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            canActivate = true; // Allow button to activate again when mouse re-enters
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            ActivateButton();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            canActivate = true; // Allow button to activate again when mouse button is released
+        }
+
+        private void ActivateButton()
         {
             if (guitarManager != null)
             {
